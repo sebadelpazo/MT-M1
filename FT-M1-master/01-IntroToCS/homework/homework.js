71e5f1bec19b01a6ae6cd6151d1fd394f2d16185
sebadelpazo <seba.delpazo@gmail.com>
2022-06-21 12:56:42 -0300

    new

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  
+  var res = 0;
+  var pos = 0;
+for (let i = num.length - 1; i >= 0; i++){
+  res = res + num[i] * 2 ** pos 
+  pos++
+}
+return res;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var res = '';
+while(num !== 0){
+  res = num % 2 + res ;
+  num = Math.floor(num/2)
+}
+return res;
 }
 
 
